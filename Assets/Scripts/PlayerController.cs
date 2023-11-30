8372c560a8765be4c48908d0d7e5aadc8c2ac3c3
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-11-30 19:50:34 +0000

    Player Animation Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,52 @@ using static UnityEngine.GraphicsBuffer;
 public class PlayerController : MonoBehaviour
 
 {
+
+    public Animator anim;
+    private Rigidbody rb;
+
     void Start()
     {
-
+        this.rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0.03f, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-0.03f, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(0.0f, 0f, -0.03f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(0.0f, 0f, 0.03f);
-        }
 
+        Pick_Up();
+        Move();
+        Idle();
+
+    }
+
+    private void Pick_Up()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            this.anim.SetBool("pick up", true);
+            //Debug.Log("pick up");//
+        }
+        else
+        {
+            this.anim.SetBool("pick up", false);
+        }
+    }
+
+    private void Move()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
+        movement.Normalize();
+
+        this.transform.position += movement * 0.01f;
+
+        this.anim.SetFloat("vertical", verticalAxis);
+        this.anim.SetFloat("horizontal", horizontalAxis);
+    }
+
+    private void Idle()
+    {
+        this.rb.AddForce(Vector3.down * 1 * Time.deltaTime, ForceMode.Impulse);
     }
 }
